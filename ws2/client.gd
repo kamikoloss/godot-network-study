02f1f2fa7213a66362771c7d02a2992aff09fa33
Kamiko Loss <info@gloxi.net>
2024-06-16 17:57:39 +0900

    WebSocketMultiplayerPeer で同期しようとしたが動かない

---
+++ ws2/client.gd
@@ -0,0 +1,39 @@
+extends Node
+
+
+#const ADDRESS = "ws://localhost:9080"
+const ADDRESS = "localhost:9080"
+
+
+@export var _player_scene: PackedScene
+@export var _network_nodes: Node2D
+
+
+var mp_peer := WebSocketMultiplayerPeer.new()
+
+
+func _ready() -> void:
+	multiplayer.peer_connected.connect(_on_peer_connected)
+	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+
+	var _error = mp_peer.create_client(ADDRESS)
+	if _error == OK:
+		print("[Client] succeeded to connect to %s" % ADDRESS)
+	else:
+		print("[Client] failed to connect to %s for %s" % [ADDRESS, _error])
+
+	multiplayer.multiplayer_peer = mp_peer
+
+
+func _on_peer_connected(id: int) -> void:
+	# Remote Peer の Player を作成する
+	print("[Client] peer (%s) is connected." % str(id))
+	var _player = _player_scene.instantiate()
+	_player.name = str(id)
+	_network_nodes.add_child(_player)
+
+
+func _on_peer_disconnected(id: int) -> void:
+	# Remote Peer の Player を破棄する
+	print("[Client] peer (%s) is disconnecred." % str(id))
+	_network_nodes.get_node(str(id)).queue_free()
---
+++ ws2/server.gd
@@ -0,0 +1,28 @@
+extends Node
+
+
+const PORT = 9090
+
+
+var mp_peer := WebSocketMultiplayerPeer.new()
+
+
+func _ready() -> void:
+	multiplayer.peer_connected.connect(_on_peer_connected)
+	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+
+	var _error = mp_peer.create_server(PORT)
+	if _error == OK:
+		print("[Server] succeeded to listen on %s" % PORT)
+	else:
+		print("[Server] failed to listen on %s for %s" % [PORT, _error])
+
+	multiplayer.multiplayer_peer = mp_peer
+
+
+func _on_peer_connected(id: int) -> void:
+	print("[Server] peer (%s) is connected." % str(id))
+
+
+func _on_peer_disconnected(id: int) -> void:
+	print("[Server] peer (%s) is disconnected." % str(id))
