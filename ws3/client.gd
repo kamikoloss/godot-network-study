e561a6c5134624ee5740115caea9470726fce617
Kamiko Loss <info@gloxi.net>
2024-06-19 01:28:27 +0900

    ping を表示するようにした

--- ws3/client.gd
+++ ws3/client.gd
@@ -4,16 +4,34 @@ extends Node2D
 const ADDRESS = "ws://localhost"
 const PORT = 8080
 
-
 @export var _web_socket_client: WebSocketClient
+
+@export var _ping_refresh_interval: float = 0.25
+
+# UI
 @export var _connect_button: Button
+@export var _ping_label: Label
+
+var _server_unix_time: float = 0.0
+var _ping_refresh_timer: float = 0.0
 
 
 func _ready() -> void:
 	_web_socket_client.connected_to_server.connect(_on_web_socket_client_connected_to_server)
 	_web_socket_client.connection_closed.connect(_on_web_socket_client_connection_closed)
 	_web_socket_client.message_received.connect(_on_web_socket_client_message_received)
-	_connect_button.pressed.connect(_on_conect_button_pressed)
+	_connect_button.pressed.connect(_on_connect_button_pressed)
+
+
+func _process(delta: float) -> void:
+	# Ping
+	if 0.0 < _server_unix_time:
+		_ping_refresh_timer += delta
+		if _ping_refresh_interval < _ping_refresh_timer:
+			_ping_refresh_timer = 0.0
+			var local_unix_time = Time.get_unix_time_from_system()
+			var ping_ms = snappedf((local_unix_time -_server_unix_time) * 1000, 0.01)
+			_ping_label.text = "Ping: %sms" % str(ping_ms)
 
 
 func _on_web_socket_client_connected_to_server():
@@ -25,10 +43,12 @@ func _on_web_socket_client_connection_closed():
 
 
 func _on_web_socket_client_message_received(message):
-	print("[Client] Message received from server. Message: %s" % [message])
+	#print("[Client] Message received from server. Message: %s" % [message])
+	var msg = JSON.parse_string(message)
+	_server_unix_time = msg["time"]
 
 
-func _on_conect_button_pressed():
+func _on_connect_button_pressed():
 	var _error = _web_socket_client.connect_to_url("%s:%s" % [ADDRESS, PORT])
 	if _error == OK:
 		print("[Client] connect_to_url OK")
--- ws3/server.gd
+++ ws3/server.gd
@@ -6,6 +6,11 @@ const PORT = 8080
 
 @export var _web_socket_server: WebSocketServer
 
+@export var _send_interval: float = 0.05
+
+
+var _send_timer: float = 0.0
+
 
 func _ready() -> void:
 	_web_socket_server.client_connected.connect(_on_web_socket_server_client_connected)
@@ -13,6 +18,17 @@ func _ready() -> void:
 	_web_socket_server.message_received.connect(_on_web_socket_server_message_received)
 
 	_start_server()
+
+
+func _process(delta: float) -> void:
+	_send_timer += delta
+	if _send_interval < _send_timer:
+		_send_timer = 0.0
+		for id in _web_socket_server.peers:
+			var msg = {
+				"time": Time.get_unix_time_from_system(),
+			}
+			_web_socket_server.send(id, JSON.stringify(msg))
 
 
 func _on_web_socket_server_client_connected(peer_id):
