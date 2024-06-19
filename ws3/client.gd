7e92b638eef2e30291a3364e1c9db5f425f48aa5
Kamiko Loss <info@gloxi.net>
2024-06-20 01:37:33 +0900

    座標を同期する (途中)

--- ws3/client.gd
+++ ws3/client.gd
@@ -5,18 +5,24 @@ const ADDRESS = "ws://localhost"
 const PORT = 8080
 
 @export var _web_socket_client: WebSocketClient
-
-@export var _ping_refresh_interval: float = 0.25
-
+@export var _player: Player
 # UI
 @export var _connect_button: Button
 @export var _ping_label: Label
 @export var _ping_avg_label: Label
 
+@export var _send_interval: float = 0.05
+@export var _ping_refresh_interval: float = 0.25
+
+
+var _send_timer: float = 0.0
 # Ping
 var _ping_refresh_timer: float = 0.0
 var _recent_ping_list: Array[float] = []
 var _recent_ping_list_max_size: int = 10
+# 接続しているプレイヤーの情報
+# { <PeerID>: { "position": <Vector2> } }
+var _players: Dictionary = {}
 
 
 func _ready() -> void:
@@ -27,14 +33,8 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	# Ping
-	if 0 < _recent_ping_list.size():
-		_ping_refresh_timer += delta
-		if _ping_refresh_interval < _ping_refresh_timer:
-			_ping_refresh_timer = 0
-			var ping_avg = _recent_ping_list.reduce(func(a, n): return a + n, 0.0) / _recent_ping_list.size()
-			_ping_label.text = "Ping: %sms" % str(snappedf(_recent_ping_list[-1] * 1000, 0.01))
-			_ping_avg_label.text = "(Avg: %sms)" % str(snappedf(ping_avg * 1000, 0.01))
+	_process_send(delta)
+	_process_refresh_ping(delta)
 
 
 func _on_web_socket_client_connected_to_server():
@@ -45,16 +45,18 @@ func _on_web_socket_client_connection_closed():
 	print("[Client] Connection closed.")
 
 
-func _on_web_socket_client_message_received(message):
+func _on_web_socket_client_message_received(message: Variant):
 	#print("[Client] Message received from server. Message: %s" % [message])
-	var msg = JSON.parse_string(message)
-	
+
+	# players
+	_players.merge(message["players"]) # TODO: PackedByteArray なので merge できない
+
 	# time
-	var ping = Time.get_unix_time_from_system() - msg["time"]
+	var ping = Time.get_unix_time_from_system() - message["time"]
 	_recent_ping_list.append(ping)
 	if _recent_ping_list_max_size < _recent_ping_list.size():
 		_recent_ping_list.pop_front()
-		print(_recent_ping_list)
+	#print(_recent_ping_list)
 
 
 func _on_connect_button_pressed():
@@ -63,3 +65,34 @@ func _on_connect_button_pressed():
 		print("[Client] connect_to_url OK")
 	else:
 		print("[Client] connect_to_url NG: %s" % error_string(_error))
+
+
+# サーバーにデータを送信する
+func _process_send(delta: float) -> void:
+	if _web_socket_client.last_state != WebSocketPeer.STATE_OPEN:
+		return
+	_send_timer += delta
+	if _send_timer < _send_interval:
+		return
+	_send_timer = 0.0
+
+	var msg = {
+		"position": _player.position,
+	}
+	_web_socket_client.send(msg)
+
+
+# Ping の表示を更新する
+func _process_refresh_ping(delta: float) -> void:
+	if _recent_ping_list.size() == 0:
+		return
+	_ping_refresh_timer += delta
+	if _ping_refresh_timer < _ping_refresh_interval:
+		return
+	_ping_refresh_timer = 0.0
+
+	var ping_avg = _recent_ping_list.reduce(func(a, n): return a + n, 0.0) / _recent_ping_list.size()
+	_ping_label.text = "Ping: %sms" % str(snappedf(_recent_ping_list[-1] * 1000, 0.01))
+	_ping_avg_label.text = "(Avg: %sms)" % str(snappedf(ping_avg * 1000, 0.01))
+
+	print(_players)
--- ws3/server.gd
+++ ws3/server.gd
@@ -10,6 +10,9 @@ const PORT = 8080
 
 
 var _send_timer: float = 0.0
+# 接続しているプレイヤーの情報
+# { <PeerID>: { "position": <Vector2> } }
+var _players: Dictionary = {}
 
 
 func _ready() -> void:
@@ -21,31 +24,41 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	_send_timer += delta
-	if _send_interval < _send_timer:
-		_send_timer = 0.0
-		for id in _web_socket_server.peers:
-			var msg = {
-				"time": Time.get_unix_time_from_system(),
-			}
-			_web_socket_server.send(id, JSON.stringify(msg))
+	_process_send(delta)
 
 
-func _on_web_socket_server_client_connected(peer_id):
+func _on_web_socket_server_client_connected(peer_id: int):
 	print("[Server] New peer connected. ID: ", peer_id)
 
 
-func _on_web_socket_server_client_disconnected(peer_id):
+func _on_web_socket_server_client_disconnected(peer_id: int):
 	print("[Server] Peer disconnected. ID: ", peer_id)
 
 
-func _on_web_socket_server_message_received(peer_id, message):
-	print("[Server] Message received from client. ID: %d, Message: %s" % [peer_id, message])
+func _on_web_socket_server_message_received(peer_id: int , message: Variant):
+	#print("[Server] Message received from client. ID: %d, Message: %s" % [peer_id, message])
+	var player_data = { peer_id: message }
+	_players.merge(player_data)
 
 
-func _start_server():
+func _start_server() -> void:
 	var _error = _web_socket_server.listen(PORT)
 	if _error == OK:
 		print("[Server] listen OK")
 	else:
 		print("[Server] listen NG: %s" % error_string(_error))
+
+
+# クライアントにデータを送信する
+func _process_send(delta: float) -> void:
+	_send_timer += delta
+	if _send_timer < _send_interval:
+		return
+	_send_timer = 0.0
+
+	for id in _web_socket_server.peers:
+		var message = {
+			"time": Time.get_unix_time_from_system(),
+			"players": var_to_bytes(_players)
+		}
+		_web_socket_server.send(id, message)
